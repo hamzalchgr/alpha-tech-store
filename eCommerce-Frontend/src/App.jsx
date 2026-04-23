d240e0112b374faebb86681451f716697e2e209e
hamzalchgr <hamzalchgrdev@gmail.com>
2026-04-23 13:13:37 +0200

    -

--- eCommerce-Frontend/src/App.jsx
+++ eCommerce-Frontend/src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Layout from "./components/layout/Layout";
-
 import Home from "./pages/Home";
+import LandingPage from './pages/LandingPage';
 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route index element={<Home />} />
+        <Route path="/landingPage/:id" element={<LandingPage />} />
       </Route>
     </Routes>
   )
--- eCommerce-Frontend/src/components/layout/Header.jsx
+++ eCommerce-Frontend/src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { NAV_LINKS } from "../../constants/links";
 
-import clsx from 'clsx';
+import clsx from "clsx";
 
 import { Menu, Search, ShoppingBag, User, X } from "lucide-react";
 
@@ -10,51 +10,63 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className={clsx(
-      "padding-x",
-      menuOpen ? "h-full" : "h-fit"
-    )}>
-      <nav className="nav-bar">
-        <div className="flex gap-5 items-center">
-          <Link className="font-black tracking-wider uppercase text-sm" to="/">
-            alpha
-          </Link>
+    <>
+      <header className={clsx("padding-x", menuOpen ? "h-full" : "h-fit")}>
+        <nav className="nav-bar">
+          <div className="flex gap-5 items-center">
+            <Link
+              className="font-black tracking-wider uppercase text-sm"
+              to="/"
+            >
+              alpha
+            </Link>
 
-          <ul className={clsx(
-            "nav-list",
-            menuOpen ? "h-full opacity-100" : "h-fit opacity-0 md:opacity-100"
-          )}>
-            {NAV_LINKS.map(({ id, label, href }) => (
-              <li key={id} className="flex items-center">
-                <Link className="nav-link" to={href}>
-                  {label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+            <ul
+              id="nav-list"
+              className={clsx(
+                "nav-list",
+                menuOpen
+                  ? "h-full opacity-100"
+                  : "h-fit opacity-0 md:opacity-100",
+              )}
+              aria-hidden={!menuOpen}
+            >
+              {NAV_LINKS.map(({ id, label, href }) => (
+                <li key={id} className="flex items-center">
+                  <Link className="nav-link" to={href}>
+                    {label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-        <div className="flex">
-          <button className="btn">
-            <Search size={20} />
-          </button>
+          <div className="flex">
+            <button className="btn">
+              <Search size={20} />
+            </button>
 
-          <Link to="/cartPage" className="btn">
-            <ShoppingBag size={20} />
-          </Link>
+            <Link to="/cartPage" className="btn">
+              <ShoppingBag size={20} />
+            </Link>
 
-          <Link to="/accountPage" className="btn">
-            <User size={20} />
-          </Link>
+            <Link to="/accountPage" className="btn">
+              <User size={20} />
+            </Link>
 
-          <button className="btn md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
-            {menuOpen ? <X size={20} /> : <Menu size={20} />}
-          </button>
-        </div>
-      </nav>
-
+            <button
+              className="btn md:hidden"
+              onClick={() => setMenuOpen(!menuOpen)}
+              aria-controls="nav-list"
+              aria-expanded={menuOpen}
+            >
+              {menuOpen ? <X size={20} /> : <Menu size={20} />}
+            </button>
+          </div>
+        </nav>
+      </header>
       <Outlet />
-    </header>
+    </>
   );
 };
 
